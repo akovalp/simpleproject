98e0f67d15dd1633935db79ae66b5639ba93179f
akovalp <87586575+akovalp@users.noreply.github.com>
2022-12-23 13:40:24 +0100

    added login page and new methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,19 +11,19 @@ class User(db.Model):
     name = db.Column(db.String(50), nullable=False)
     surname = db.Column(db.String(50), nullable=False)
     birthday = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False , unique=True)
+    password = db.Column(db.String(50), nullable=False , unique=True)
 
-    def __init__(self, name, surname, birthday, email):
+    def __init__(self, name, surname, birthday, email, password):
         self.name = name
         self.surname = surname
         self.birthday = birthday
         self.email = email
+        self.password = password
     
     def __repr__(self):
-        return f"<id={self.id}, name={self.name}> surname={self.surname}> birthday={self.birthday}> email={self.email}>"
+        return f"id={self.id} \n name={self.name} \n surname={self.surname} \n birthday={self.birthday} \n email={self.email} \n password={self.password} \n"
     
-
-
     
 db.create_all()
 
@@ -41,10 +41,11 @@ def page():
         surname = request.form['surname']
         birthday = request.form['birthday']
         email = request.form['email']
-        new_user = User(name=name, surname=surname, birthday=birthday, email=email)
+        password = request.form['password']
+        new_user = User(name=name, surname=surname, birthday=birthday, email=email , password=password)
         db.session.add(new_user)
         db.session.commit()
-        return render_template('thankyou.html', name=name, surname=surname, birthday=birthday, email=email)
+        return render_template('thankyou.html', name=name, surname=surname, birthday=birthday, email=email , password=password)
     
     return render_template('form.html')
 
@@ -58,13 +59,28 @@ def thankyou():
         name = request.args.get('name')
         surname = request.args.get('surname')
         email = request.args.get('email')
+        password = request.args.get('password')
         
-        return render_template('thankyou.html', name=name, surname=surname, all_users=all_users, email=email)
+        return render_template('thankyou.html', name=name, surname=surname, all_users=all_users, email=email, password=password)
 
 @app.route('/users')
 def users():
     all_users = User.query.all()
     return render_template('userlists.html', all_users=all_users)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email, password=password).first()
+        
+        if user:
+            return redirect(url_for('users'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
